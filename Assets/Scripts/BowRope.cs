b0cdacd196f0ae3e41a55042634553c4f1d92cfa
tathara <sarturbusiness@gmail.com>
2023-03-13 14:24:45 +0300

    new shooting system :D

--- Assets/Scripts/BowRope.cs
+++ Assets/Scripts/BowRope.cs
@@ -17,39 +17,14 @@ public class BowRope : MonoBehaviour
     public AnimationCurve RopeReturnAnimation;
 
     public float ReturnTime;
+    public float arrowSpeed;
 
     public Arrow CurrentArrow;
 
     public AudioSource BowTension;
     public AudioSource ArrowWhistling;
 
-    public Vector3 r = new(0f, 0f, 0f);
-    public Vector3 v = new();
-    public Vector3 g = new(0, 9.81f, 0);
-    public Vector3 wind = new();
 
-    public double v0;
-    public float fi;
-    public float windx, windy, windz, h;
-    public double vmax = 150;
-
-    public double Modul(Vector3 a)
-    { return Math.Sqrt(a.x * a.x + a.y * a.y + a.z * a.z); }
-    private Vector3 Accel(Vector3 g, Vector3 v, double vmax, Vector3 wind, double h)
-    {
-        return g - (float)(Math.Exp(-h / 10000) * Modul(g) * Modul(v - wind) / (vmax * vmax)) * (v - wind);
-    }
-    private void VerletPredKorr(Vector3 g, ref Vector3 r, ref Vector3 v, double vmax, Vector3 wind)
-    {
-        Vector3 a;
-        Vector3 vPred;
-        a = Accel(g, v, vmax, wind, r.y);
-        r += v * Time.deltaTime + (float)0.5 * Time.deltaTime * Time.deltaTime * a;
-        vPred = v + a * Time.deltaTime;
-        v += (float)0.5 * Time.deltaTime * a;
-        a = Accel(g, vPred, vmax, wind, r.y);
-        v += (float)0.5 * Time.deltaTime * a;
-    }
     void Start()
     {
         RopeNearLocalPosition = RopeTransform.localPosition;
@@ -62,10 +37,8 @@ public class BowRope : MonoBehaviour
         {
             _pressed = true;
 
-            r = new Vector3(0f, 0f, 0f);
-            v = new Vector3(0f, 0f, 0f);
+            
             CurrentArrow.SetToRope(RopeTransform);
-
             BowTension.pitch = UnityEngine.Random.Range(0.8f, 1.2f);
             BowTension.Play();
         }
@@ -74,16 +47,8 @@ public class BowRope : MonoBehaviour
             fly = true;
             _pressed = false;
             StartCoroutine(RopeReturn());
-            CurrentArrow.Shot();
-            CurrentArrow.transform.localEulerAngles = new Vector3(CurrentArrow.transform.parent.eulerAngles.x, 0, CurrentArrow.transform.parent.eulerAngles.z);
-            CurrentArrow.transform.parent.eulerAngles = new Vector3(0, CurrentArrow.transform.parent.eulerAngles.y, 0);
-            fi = -CurrentArrow.transform.localEulerAngles.x;
-            fi *= Mathf.Deg2Rad;
-            v.x = (float)(v0 * Tension * Mathf.Cos(fi));
-            v.y = (float)(v0 * Tension * Mathf.Sin(fi));
-            wind.x = windx;
-            wind.y = windy;
-            wind.z = windz;
+            CurrentArrow.Shot(arrowSpeed * Tension);
+
 
             Tension = 0;
 
@@ -113,17 +78,5 @@ public class BowRope : MonoBehaviour
         }
         RopeTransform.localPosition = RopeNearLocalPosition;
 
-    }
-
-    void FixedUpdate()
-    {
-        if (!_pressed && fly)
-        {
-            VerletPredKorr(g, ref r, ref v, vmax, wind);
-            Vector3 NextPos = new(CurrentArrow.transform.localPosition.x, r.y, r.x);
-            CurrentArrow.transform.rotation = Quaternion.LookRotation(-(CurrentArrow.transform.localPosition - NextPos));
-            CurrentArrow.transform.eulerAngles += new Vector3(0, CurrentArrow.transform.parent.eulerAngles.y, 0);
-            CurrentArrow.transform.localPosition = NextPos;
-        }
     }
 }
--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransform;
     float rotationX = 0f;
     float rotationY = 0f;
 
@@ -15,13 +17,22 @@ public class CameraMovement : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
+
     void Update()
     {
+        Vector3 mouseWorldPosition = Vector3.zero;
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if(Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask)) {
+            debugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+        }
         rotationY += Input.GetAxis("Mouse X") * sensitivity;
         rotationX += Input.GetAxis("Mouse Y") * -1 * sensitivity;
         rotationX = Mathf.Clamp(rotationX, -90f, 90f);
         rotationY = Mathf.Clamp(rotationY, -90f, 90f);
         transform.localEulerAngles = new Vector3(rotationX, 90 + rotationY, 0);
     }
-
+    
+    
 }
